41c43c4dd70aee249c8c5e2bddb9ee489a3baf89
Julia Florea <julia5florea@gmail.com>
2023-11-23 21:01:32 +0200

    Added Medical Record Migration

---
+++ src/js/main.js
@@ -0,0 +1,260 @@
+
+
+
+
+const contractAddress = "0xa3917Fab5eB4C803fF1495CB881f0359Be65715B";
+const contractAbi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "healthcareProviders",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "providerDataHash",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "patientList",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "patients",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "recordHash",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "practitionerList",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "practitioners",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "practitionerDataHash",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "providerList",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_recordHash",
+          "type": "string"
+        }
+      ],
+      "name": "addPatient",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_practitionerDataHash",
+          "type": "string"
+        }
+      ],
+      "name": "addPractitioner",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_providerDataHash",
+          "type": "string"
+        }
+      ],
+      "name": "addHealthcareProvider",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]; 
+
+// Create a web3 instance
+
+const Web3 = require('web3');
+const Web3HttpProvider = require('web3-providers-http');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+// Load the contract
+const medicalRecordsContract = new web3.eth.Contract(contractAbi, contractAddress);
+
+// Function to add a patient
+async function addPatient() {
+    const patientName = $("#patientName").val();
+    const patientAge = $("#patientAge").val();
+    const patientRecordHash = "QmNTGuQDdX547mZRqGx1oHHfCJN4pXNkFaq2Lq2qGw6CFC"; 
+
+    // Get the user's account
+    const accounts = await web3.eth.getAccounts();
+    const userAccount = accounts[0];
+
+    try {
+        // Call the smart contract function to add a patient
+        await medicalRecordsContract.methods
+            .addPatient(patientName, patientAge, patientRecordHash)
+            .send({ from: userAccount });
+
+        alert("Patient added successfully!");
+    } catch (error) {
+        console.error("Error adding patient:", error);
+        alert("Error adding patient. Check the console for details.");
+    }
+}
+
+
